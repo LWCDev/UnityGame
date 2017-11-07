43d51222d89b9c88969f98ff8a8dc3f7e1892178
Legoman78787 <lewiswelshclark787@gmail.com>
2017-11-07 17:56:44 +0000

    h

---
+++ Coursework/Assets/Scripts/TriangleRemove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TriangleRemove : MonoBehaviour {
+    public GameObject scroll;
+    public GameObject sprite;
+    SpriteRenderer Triangle;
+    Scrollbar ScrollBar;
+    Scrollbar scrollnum;
+	// Use this for initialization
+	void Start () {
+        scrollnum = scroll.GetComponent<Scrollbar>();
+        Triangle = sprite.GetComponent<SpriteRenderer>();
+        ScrollBar = scroll.GetComponent<Scrollbar>();
+    }
+	
+	// Update is called once per frame
+	
+    public void SpriteRemove()
+    {
+        if (scrollnum.value < 0.3)
+        {
+            Triangle.enabled = false;
+        }
+        else
+        {
+            Triangle.enabled = true;
+        }
+    }
+}
